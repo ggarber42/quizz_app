c78c7eba117363a09669ca303eda443f6efb0890
ggarber42 <ggarber@protonmail.com>
2021-12-19 10:15:56 -0300

    lesson: invoking named functions

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,29 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answered');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
-      home: Text('Jadeeeee')
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Quizz app'),
+      ),
+      body: Column(
+        children: [
+          Text('The question!'),
+          RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+          RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+          RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
+        ],
+      ),
+    ));
   }
 }
-
